93a7942f74ecce1a7ad26a9d829a48acd91ff05e
ricky10099 <rickyto10099@gmail.com>
2025-01-24 08:24:17 +0900

    Adding Wireframe pipeline

---
+++ RavaEngineCore/src/Framework/Vulkan/RenderSystem/WireframeRenderSystem.cpp
@@ -0,0 +1,12 @@
+#include "ravapch.h"
+
+#include "Framework/Vulkan/RenderSystem/WireframeRenderSystem.h"
+#include "Framework/Vulkan/VKUtils.h"
+#include "Framework/Components.h"
+
+namespace Vulkan {
+	WireframeRenderSystem::WireframeRenderSystem(VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout) {
+
+}
+
+}
---
+++ RavaEngineCore/src/Framework/Vulkan/RenderSystem/WireframeRenderSystem.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Framework/Vulkan/Pipeline.h"
+
+namespace Vulkan {
+class WireframeRenderSystem {
+   public:
+	WireframeRenderSystem(VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout);
+	~WireframeRenderSystem();
+
+	NO_COPY(WireframeRenderSystem)
+
+	//void Update(FrameInfo& frameInfo, GlobalUbo& ubo, entt::registry& registry);
+	void Render(FrameInfo& frameInfo, entt::registry& registry);
+
+   private:
+	void CreatePipelineLayout(VkDescriptorSetLayout globalSetLayout);
+	void CreatePipeline(VkRenderPass renderPass);
+
+	Unique<Pipeline> m_pipeline;
+	VkPipelineLayout m_pipelineLayout;
+};
+}  // namespace Vulkan
